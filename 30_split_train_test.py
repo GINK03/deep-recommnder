f7c2acfee3c384d625029cd9ec79f21777c95d1e
gimpei <gim.kobayashi@gmail.com>
2019-02-12 02:14:26 +0900

    update

--- 30_split_train_test.py
+++ 30_split_train_test.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import random
-
+import numpy as np
 df = pd.read_csv('works/dataset/preprocess.csv')
 
-userIds = list( df['userId'].unique() )
-testIds  = set(random.sample(userIds, len(userIds)//5))
+userIds = list(df['userId'].unique())
+testIds = set(random.sample(userIds, len(userIds)//5))
 trainIds = set(userId for userId in userIds if userId not in testIds)
 
-df[df['userId'].apply(lambda x:x in testIds)].to_csv('works/dataset/test.csv', index=None)
-df[df['userId'].apply(lambda x:x in trainIds)].to_csv('works/dataset/train.csv', index=None)
+df[df['userId'].apply(lambda x:x in testIds)].to_csv(
+    'works/dataset/test.csv', index=None)
 
+for index, chunk_train_ids in enumerate(np.array_split(list(trainIds), 10)):
+    print('make trian dataset', index)
+    chunks = set(chunk_train_ids.tolist())
+    df[df['userId'].apply(lambda x:x in chunks)].to_csv(
+        f'works/dataset/train_{index:03d}.csv', index=None)
---
+++ 40_make_sparse_matrix.py
@@ -0,0 +1,28 @@
+import numpy as np
+from scipy.sparse import csr_matrix as Sparse
+import pandas as pd
+import json
+import pickle
+import gzip
+import glob
+from scipy.sparse import save_npz
+
+user_index = json.load(open('works/defs/user_index.json'))
+movie_index = json.load(open('works/defs/smovie_index.json'))
+
+
+for TYPE in ['train', 'test']:
+	for index, fn in enumerate(glob.glob(f'./works/dataset/{TYPE}*.csv')):
+		df = pd.read_csv(fn)
+		userIds = df['userId'].unique()
+		print(len(userIds))
+		sparse = Sparse((len(userIds), len(movie_index)),
+												dtype=np.float).toarray()
+
+		for masterIndex, (userId, subDf) in enumerate(df.groupby(by=['userId'])):
+				print(userId)
+				for movieId, score in zip(subDf['movieId'].tolist(), subDf['score'].tolist()):
+						mindex = movie_index[str(movieId)]
+						sparse[masterIndex, mindex] = float(score)
+		print('try to compress')
+		np.savez(f'works/dataset/{TYPE}_{index:02d}', sparse=sparse)
